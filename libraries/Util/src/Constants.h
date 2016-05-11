7c0ae4904747105d2a9b523687cac3d4a83bcb1f
Dmitry Konishchev <konishchev@gmail.com>
2016-05-11 22:14:04 +0300

    Add thermometer experiment

---
+++ libraries/Util/src/Constants.h
@@ -0,0 +1,17 @@
+#ifndef Util_Constants_h
+#define Util_Constants_h
+
+namespace Util { namespace Constants {
+
+const int VOLTS = 5;
+
+const int PWM_LOW = 0;
+const int PWM_HIGH = 255;
+
+const int ANALOG_LOW = 0;
+const int ANALOG_HIGH = 1023;
+
+}}
+
+#endif
+
