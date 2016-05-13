b15221c2cf016653a29993371d26befa29e758d6
Dmitry Konishchev <konishchev@gmail.com>
2016-05-13 21:30:05 +0300

    Revert "Use min()/max() from Arduino.h for better compatibility"
    This reverts commit 27717657be07415805993fb6a95868932c2a3c88.

--- libraries/Util/src/Util/Mocks.hpp
+++ libraries/Util/src/Util/Mocks.hpp
@@ -2,12 +2,10 @@
 #ifndef Util_Mocks_hpp
 #define Util_Mocks_hpp
 
-// Backported from Arduino.h
-#define min(a,b) ((a)<(b)?(a):(b))
-#define max(a,b) ((a)>(b)?(a):(b))
-#define abs(x) ((x)>0?(x):-(x))
-#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
-#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#include <algorithm>
+
+using std::min;
+using std::max;
 
 unsigned long millis();
 void delay(unsigned long time);
