f7c46e8680e79901ae832433f703f3e90ef6cf19
Dmitry Konishchev <konishchev@gmail.com>
2016-05-13 09:11:03 +0300

    Add LinkedNodeList module

---
+++ libraries/Util/src/Util/LinkedNodeList.hpp
@@ -0,0 +1,60 @@
+#ifndef Util_LinkedNodeList_hpp
+#define Util_LinkedNodeList_hpp
+
+#ifdef ARDUINO
+    #include <Arduino.h>
+#else
+    #include <cstddef>
+#endif
+
+#include "Assertion.hpp"
+#include "TypeTraits.hpp"
+
+namespace Util {
+
+template <class T>
+class LinkedNodeList: TypeTraits::NonCopyable {
+    public:
+        T* prev() const {
+            return prev_;
+        }
+
+        T* next() const {
+            return next_;
+        }
+
+        void insert(T* node) {
+            UTIL_ASSERT(!node->prev_ && !node->next_);
+            node->prev_ = static_cast<T*>(this);
+            node->next_ = this->next_;
+            this->next_ = node;
+        }
+
+        bool remove(T** next) {
+            bool first = prev_ == nullptr;
+            *next = next_;
+
+            if(next_)
+                next_->prev_ = prev_;
+
+            if(prev_)
+                prev_->next_ = next_;
+
+            prev_ = nullptr;
+            next_ = nullptr;
+
+            return first;
+        }
+
+    protected:
+        LinkedNodeList(): prev_(nullptr), next_(nullptr) {
+        }
+
+    private:
+        T* prev_;
+        T* next_;
+};
+
+}
+
+#endif
---
+++ libraries/Util/tests/LinkedNodeList.cpp
@@ -0,0 +1,57 @@
+#define BOOST_TEST_MODULE LinkedNodeList
+#include <boost/test/included/unit_test.hpp>
+
+#include <cstddef>
+#include <vector>
+
+#include <Util/LinkedNodeList.hpp>
+
+using namespace Util;
+
+struct Node: public LinkedNodeList<Node> {
+    int value;
+
+    Node(int value): value(value) {
+    }
+};
+
+std::vector<int> traverse(Node* node) {
+    std::vector<int> values;
+
+    while(node) {
+        values.push_back(node->value);
+        node = node->next();
+    }
+
+    return values;
+}
+
+void compare(Node* nodes, std::vector<int> expected) {
+    std::vector<int> list = traverse(nodes);
+    BOOST_REQUIRE_EQUAL_COLLECTIONS(list.begin(), list.end(), expected.begin(), expected.end());
+}
+
+BOOST_AUTO_TEST_CASE(insert_remove) {
+    Node one(1), two(2), three(3), four(4);
+
+    Node* nodes = &one;
+    Node* next;
+
+    one.insert(&two);
+    two.insert(&four);
+    two.insert(&three);
+    compare(nodes, {1, 2, 3, 4});
+
+    BOOST_REQUIRE(!three.remove(&next));
+    BOOST_REQUIRE(next == &four);
+    compare(nodes, {1, 2, 4});
+
+    BOOST_REQUIRE(!four.remove(&next));
+    BOOST_REQUIRE(next == nullptr);
+    compare(nodes, {1, 2});
+
+    BOOST_REQUIRE(one.remove(&next));
+    BOOST_REQUIRE(next == &two);
+    nodes = next;
+    compare(nodes, {2});
+}
