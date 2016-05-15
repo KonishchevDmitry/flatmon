2841781351eee9fdbfe059061b4dbb1653bce404
Dmitry Konishchev <konishchev@gmail.com>
2016-05-15 17:03:34 +0300

    Add logging module

---
+++ libraries/Util/src/Util/Logging.hpp
@@ -0,0 +1,64 @@
+#ifndef Util_Logging_hpp
+#define Util_Logging_hpp
+
+#ifndef UTIL_ENABLE_LOGGING
+    #error UTIL_ENABLE_LOGGING macro must be defined to use logging module.
+#endif
+
+#include <Arduino.h>
+
+namespace Util { namespace Logging {
+
+inline void init() {
+#if UTIL_ENABLE_LOGGING
+    Serial.begin(9600);
+#endif
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+template <typename A1>
+void log(A1 a1) {
+#if UTIL_ENABLE_LOGGING
+    Serial.println(a1);
+#endif
+}
+
+template <typename A1, typename A2>
+void log(A1 a1, A2 a2) {
+#if UTIL_ENABLE_LOGGING
+    Serial.print(a1);
+    log(a2);
+#endif
+}
+
+template <typename A1, typename A2, typename A3>
+void log(A1 a1, A2 a2, A3 a3) {
+#if UTIL_ENABLE_LOGGING
+    Serial.print(a1);
+    log(a2, a3);
+#endif
+}
+
+template <typename A1, typename A2, typename A3, typename A4>
+void log(A1 a1, A2 a2, A3 a3, A4 a4) {
+#if UTIL_ENABLE_LOGGING
+    Serial.print(a1);
+    log(a2, a3, a4);
+#endif
+}
+
+template <typename A1, typename A2, typename A3, typename A4, typename A5>
+void log(A1 a1, A2 a2, A3 a3, A4 a4, A5 a5) {
+#if UTIL_ENABLE_LOGGING
+    Serial.print(a1);
+    log(a2, a3, a4, a5);
+#endif
+}
+
+#pragma GCC diagnostic pop
+
+}}
+
+#endif
