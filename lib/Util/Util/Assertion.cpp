a2934d55f87f36cee15b137c6f86f0a43c624b9d
Dmitry Konishchev <konishchev@gmail.com>
2016-06-25 18:03:05 +0300

    Enable verbose assertions

--- lib/Util/Util/Assertion.cpp
+++ lib/Util/Util/Assertion.cpp
@@ -8,21 +8,29 @@
 
 namespace Util {
 
-void abort() {
-    #ifdef ARDUINO
-        log(F("Assertion error. Stopping the device."));
+#if UTIL_VERBOSE_ASSERTS
+    void abort(const char* file, int line) {
+#else
+    void abort() {
+#endif
+        #ifdef ARDUINO
+            #if UTIL_VERBOSE_ASSERTS
+                log(F("Assertion error at "), file, F(":"), line, F(". Stopping the device."));
+            #else
+                log(F("Assertion error. Stopping the device."));
+            #endif
 
-        bool state = false;
-        pinMode(LED_BUILTIN, OUTPUT);
+            bool state = false;
+            pinMode(LED_BUILTIN, OUTPUT);
 
-        while(true) {
-            state = !state;
-            digitalWrite(LED_BUILTIN, state);
-            delay(500);
-        }
-    #else
-        ::abort();
-    #endif
-}
+            while(true) {
+                state = !state;
+                digitalWrite(LED_BUILTIN, state);
+                delay(500);
+            }
+        #else
+            ::abort();
+        #endif
+    }
 
 }
--- lib/Util/Util/Assertion.hpp
+++ lib/Util/Util/Assertion.hpp
@@ -3,14 +3,25 @@
 
 #include "Core.hpp"
 
-#define UTIL_ASSERT(expression) do { \
-    if(!(expression))                \
-        ::Util::abort();             \
-} while(false)
+#if UTIL_VERBOSE_ASSERTS
+    #define UTIL_ASSERT(expression) do {       \
+        if(!(expression))                      \
+            ::Util::abort(__FILE__, __LINE__); \
+    } while(false)
+#else
+    #define UTIL_ASSERT(expression) do { \
+        if(!(expression))                \
+            ::Util::abort();             \
+    } while(false)
+#endif
 
 namespace Util {
 
-void abort() __attribute__((__noreturn__));
+#if UTIL_VERBOSE_ASSERTS
+    void abort(const char* file, int line) __attribute__((__noreturn__));
+#else
+    void abort() __attribute__((__noreturn__));
+#endif
 
 }
 
