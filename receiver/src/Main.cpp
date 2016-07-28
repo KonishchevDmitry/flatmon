ec5d2bdf5d4b613016d579a1b3d79c098ce0a067
Dmitry Konishchev <konishchev@gmail.com>
2016-07-28 21:41:20 +0300

    Fix byte order in sending message

--- receiver/src/Main.cpp
+++ receiver/src/Main.cpp
@@ -77,6 +77,7 @@ void setup() {
 
         SensorMessage* message = reinterpret_cast<SensorMessage*>(sensorMessageBuf);
         log(F("Got a message from #"), message->sensorId, F(" sensor."));
+        message->co2Concentration = htons(message->co2Concentration);
         sendMessageToServer(message, sizeof(SensorMessage));
     }
 }
