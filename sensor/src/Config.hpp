73b398c1580d9fded0ef2e7d153072334c01817b
Dmitry Konishchev <konishchev@gmail.com>
2016-08-20 13:28:17 +0300

    Alter config values

--- sensor/src/Config.hpp
+++ sensor/src/Config.hpp
@@ -1,12 +1,13 @@
 #ifndef Config_hpp
 #define Config_hpp
 
-#define CONFIG_ENABLE_TRANSMITTER 0
+#define CONFIG_ENABLE_TRANSMITTER 1
 #define CONFIG_CO2_SENSOR_USE_SOFTWARE_SERIAL 0
-#define CONFIG_CO2_PWM_SENSOR_CYCLE_DURATION 1004408
+//#define CONFIG_CO2_PWM_SENSOR_CYCLE_DURATION 1004408
+#define CONFIG_CO2_PWM_SENSOR_CYCLE_DURATION 1004000
 
 namespace Config {
-    enum class SensorId: uint8_t {little_room};
+    enum class SensorId: uint8_t {little_room, big_room};
     constexpr uint8_t SENSOR_ID = uint8_t(SensorId::little_room);
     constexpr double CO2_PWM_SENSOR_TIME_SCALE_FACTOR = double(CONFIG_CO2_PWM_SENSOR_CYCLE_DURATION) / 1004000;
 }
