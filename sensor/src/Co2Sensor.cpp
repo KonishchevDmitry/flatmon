5bee960c58614a5429a52f48fd0fed376a369c6f
Dmitry Konishchev <konishchev@gmail.com>
2016-09-10 17:00:27 +0300

    Switch to Arduino Mega

--- sensor/src/Co2Sensor.cpp
+++ sensor/src/Co2Sensor.cpp
@@ -4,6 +4,7 @@
 #include <Util/Logging.hpp>
 
 #include "Co2Sensor.hpp"
+#include "Config.hpp"
 
 using Util::Logging::log;
 using Util::MonotonicTime;
@@ -20,7 +21,7 @@ namespace {
     // It shouldn't be polled more often then once in 10 seconds via UART because in that case it returns strange
     // results.
     const auto POLLING_PERIOD = 10 * Constants::SECOND_MILLIS;
-    const auto PREHEAT_TIME = DEBUG_MODE ? POLLING_PERIOD : 3 * Constants::MINUTE_MILLIS;
+    const auto PREHEAT_TIME = CONFIG_DEBUG_MODE ? POLLING_PERIOD : 3 * Constants::MINUTE_MILLIS;
 
     Comfort getComfort(Concentration concentration) {
         // CO2 levels explained:
--- sensor/src/Config.hpp
+++ sensor/src/Config.hpp
@@ -1,6 +1,10 @@
 #ifndef Config_hpp
 #define Config_hpp
 
+#ifndef CONFIG_DEBUG_MODE
+    #define CONFIG_DEBUG_MODE 0
+#endif
+
 #define CONFIG_ENABLE_TRANSMITTER 1
 #define CONFIG_CO2_SENSOR_USE_SOFTWARE_SERIAL 0
 #define CONFIG_CO2_PWM_SENSOR_CYCLE_DURATION 1004000
