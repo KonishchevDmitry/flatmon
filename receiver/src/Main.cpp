cd649c1b0854a108afed0104565c51f727c91d8e
Dmitry Konishchev <konishchev@gmail.com>
2016-09-14 20:46:41 +0300

    Support log levels

--- receiver/src/Main.cpp
+++ receiver/src/Main.cpp
@@ -9,7 +9,10 @@
 
 namespace Constants = Util::Constants;
 
-using Util::Logging::log;
+using Util::Logging::log_error;
+using Util::Logging::log_warning;
+using Util::Logging::log_info;
+
 using Common::SensorMessage;
 
 namespace {
@@ -31,7 +34,7 @@ void setup() {
     if(!RECEIVER.init())
         UTIL_LOGICAL_ERROR(F("Failed to initialize the receiver."));
 
-    log(F("Listening to messages from sensors..."));
+    log_info(F("Listening to messages from sensors..."));
 
     TimeMillis lastStatsTime = millis();
     TimeMillis lastHeartbeatTime = millis();
@@ -47,12 +50,13 @@ void setup() {
             bool noMessagesTimeoutExceeded = curTime - lastHeartbeatTime >= Constants::MINUTE_MILLIS;
 
             if(noMessagesTimeoutExceeded) {
-                log(F("I'm alive but there are no messages from sensors."));
+                log_warning(F("I'm alive but there are no messages from sensors."));
                 lastHeartbeatTime = curTime;
             }
 
             if(noMessagesTimeoutExceeded || curTime - lastStatsTime >= Constants::MINUTE_MILLIS) {
-                log(F("Received messages statistics: "), RECEIVER.rxGood(), F(" good, "), RECEIVER.rxBad(), F(" bad."));
+                log_info(F("Received messages statistics: "),
+                         RECEIVER.rxGood(), F(" good, "), RECEIVER.rxBad(), F(" bad."));
                 lastStatsTime = curTime;
             }
 
@@ -62,20 +66,20 @@ void setup() {
         lastHeartbeatTime = millis();
 
         if(messageSize != sizeof(SensorMessage)) {
-            log(F("Got a message with invalid size."));
+            log_error(F("Got a message with invalid size."));
             continue;
         }
 
         SensorMessage* message = reinterpret_cast<SensorMessage*>(sensorMessageBuf);
         if(message->messageType != SensorMessage::MESSAGE_TYPE) {
-            log(F("Got a message with invalid message type."));
+            log_error(F("Got a message with invalid message type."));
             continue;
         }
 
-        log(F("Got a message from #"), message->sensorId, F(" sensor:"));
+        log_info(F("Got a message from #"), message->sensorId, F(" sensor:"));
         uint16_t checksum = uint16_t(message->sensorId) + message->temperature + message->humidity +
                             message->co2Concentration + message->pressure;
-        log(F("> "), message->sensorId, ",", message->temperature, ",", message->humidity, ",",
+        log_info(F("> "), message->sensorId, ",", message->temperature, ",", message->humidity, ",",
             message->co2Concentration, ",", message->pressure, ",", checksum);
     }
 }
